6b926614c2768035da55da5b4db346e49f439e41
Hiren Vadher <hirenvadher954@gmail.com>
2019-12-05 21:12:08 +0530

    Create Bon Appétit.py

---
+++ Bon Appétit.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the bonAppetit function below.
+def bonAppetit(bill, k, b):
+    bill.remove(bill[k])
+    addition = sum(bill)/2
+    if addition == b:
+        print("Bon Appetit")
+    else:
+        print(int(b-addition))    
+
+
+
+if __name__ == '__main__':
+    nk = input().rstrip().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
