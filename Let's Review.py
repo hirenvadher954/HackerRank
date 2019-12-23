80032b675ba4bae72ce59a0a38b6fa1950dc93eb
Hiren Vadher <hirenvadher954@gmail.com>
2019-12-23 22:15:45 +0530

    Create Let's Review.py

---
+++ Let's Review.py
@@ -0,0 +1,20 @@
+#Given a string,S, of length N that is indexed from 0 to N-1 , print its even-indexed and odd-indexed characters as 2 space-separated strings on a single line (see the Sample below for more detail).
+
+
+N = int(input())
+
+for i in range(0, N):
+
+    string = input()
+
+    for j in range(0, len(string)):
+        if j % 2 == 0:
+            print(string[j], end='')
+
+    print(" ", end='')
+
+    for j in range(0, len(string)):
+        if j % 2 != 0:
+            print(string[j], end='')
+
+    print("")
