1133c3f447c45c621115283cd4dfa27684b44bb2
Hiren Vadher <hirenvadher954@gmail.com>
2019-12-31 20:24:22 +0530

    Create Chocolate Feast.py

---
+++ Chocolate Feast.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the chocolateFeast function below.
+def chocolateFeast(n, c, m):
+    count = n//c
+    curr = count
+    while curr>=m:
+        kagaj = curr//m
+        count += kagaj
+        curr = kagaj + curr%m
+    return count    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        ncm = input().split()
+
+        n = int(ncm[0])
+
+        c = int(ncm[1])
+
+        m = int(ncm[2])
+
+        result = chocolateFeast(n, c, m)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
